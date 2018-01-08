d529fb08804302a2c6b1fb26b1fd82a6d1b0c9ec
Vincent Chho <vincec362@gmail.com>
2018-01-07 23:11:57 -0800

    Revised my JS file and edited my HTML file for cleaner code

--- js/script.js
+++ js/script.js
@@ -1,80 +1,68 @@
 let text = document.getElementById('text');
 
-document.getElementById("button1").addEventListener('click', function () {
-  console.log(parseInt(text.value += 1));
-});
+let tom = document.querySelectorAll('.btn');
 
-document.getElementById("button2").addEventListener('click', function () {
-  text.value += 2;
-});
-
-document.getElementById("button3").addEventListener('click', function () {
-  text.value += 3;
-});
-
-document.getElementById("button4").addEventListener('click', function () {
-  text.value += 4;
-});
-
-document.getElementById("button5").addEventListener('click', function () {
-  text.value += 5;
-});
-
-document.getElementById("button6").addEventListener('click', function () {
-  text.value += 6;
-});
-
-document.getElementById("button7").addEventListener('click', function () {
-  text.value += 7;
-});
-
-document.getElementById("button8").addEventListener('click', function () {
-  parseInt(text.value += 8);
-});
-
-document.getElementById("button9").addEventListener('click', function () {
-  console.log(parseInt(text.value += 9));
-});
-
-document.getElementById("button0").addEventListener('click', function () {
-  console.log(parseInt(text.value += 0));
-});
-
-document.getElementById("eraseText").addEventListener('click', function () {
-  text.value = '';
-});
-
-document.getElementById("addButton").addEventListener('click', function () {
-  console.log(text.value += ' + ');
-});
-
-document.getElementById('evaluate').addEventListener('click', function () {
-  let answer = text.value;
-  answer = eval(answer);
-  text.value = answer;
-});
-
-document.getElementById('divide').addEventListener('click', function () {
-  text.value += "/";
-});
-
-document.getElementById('subtract').addEventListener('click', function () {
-  text.value += "-";
-});
-
-document.getElementById('multiply').addEventListener('click', function () {
-  text.value += "*";
-});
-
-
-// var erased = document.querySelector('#eraseText');
-// var input = document.querySelector('#text');
-
-// erased.addEventListener('click', function(){
-//   input.value = '';
-// }, false);
-
-// function eraseText() {
-//     document.getElementById('text').value = "";
-// }
-
+for (let i = 0; i < tom.length; i++) {
+  // console.log(tom[i]);
+  tom[i].addEventListener('click', display);
+  
+  function display() {
+    // console.log(text.value += 1);
+    switch (tom[i]) {
+      case tom[0]:
+        text.value += 1;
+        console.log('1');
+        break;
+      case tom[1]:
+        text.value += 2;
+        console.log('it worked');
+        break;
+      case tom[2]:
+        text.value += 3;
+        break;
+      case tom[3]:
+        text.value += '+';
+        break;
+      case tom[4]:
+        text.value += 4;
+        break;
+      case tom[5]:
+        text.value += 5;
+        break;
+      case tom[6]:
+        text.value += 6;
+        break;
+      case tom[7]:
+        text.value += '-';
+        break;
+      case tom[8]:
+        text.value += 7;
+        break;
+      case tom[9]:
+        text.value += 8;
+        break;
+      case tom[10]:
+        text.value += 9;
+        break;
+      case tom[11]:
+        text.value += '*';
+        break;
+      case tom[12]:
+        text.value += 0;
+        break;
+      case tom[13]:
+        text.value = '';
+        break;
+      case tom[14]:
+        text.value += '/';
+        break;
+      case tom[15]:
+        let answer = text.value;
+        answer = eval(answer);
+        text.value = answer;
+        break;
+      default:
+        break;
+    }
+  };
+};
